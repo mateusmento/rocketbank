040a72206c1bc82e15f6fde754af23a6f22c7596
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-05-15 15:36:09 -0300

    Add stubbed user resource module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
 import { ClientModule } from "./client/client.module";
+import { UserModule } from './user/user.module';
 
 @Module({
-	imports: [ClientModule],
+	imports: [ClientModule, UserModule],
 	controllers: [],
 	providers: [],
 })
