5e3f51e7682627d97e25294c014868abb8f38a37
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-05-15 19:28:05 -0300

    Import typeorm module in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from "@nestjs/common";
 import { ClientModule } from "./client/client.module";
 import { UserModule } from "./user/user.module";
 import { AuthModule } from "./auth/auth.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-	imports: [ClientModule, UserModule, AuthModule],
+	imports: [
+		TypeOrmModule.forRoot({
+			type: "postgres",
+			name: "postgres",
+			host: "localhost",
+			port: 5432,
+			username: "rocket_bank",
+			password: "kl1jh2h3k1",
+			database: "rocket_bank",
+		}),
+		ClientModule,
+		UserModule,
+		AuthModule,
+	],
 	controllers: [],
 	providers: [],
 })
