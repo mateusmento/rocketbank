5c45a9fd70f97fa866ebb745bb6345a88a456898
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-05-15 15:50:30 -0300

    Encrypt user password on creation

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from "@nestjs/common";
 import { CreateUserDto } from "./dto/create-user.dto";
+import * as bcrypt from "bcrypt";
 
 let sequenceId = 1;
 const users = [];
 
 @Injectable()
 export class UserService {
-	create(createUserDto: CreateUserDto) {
+	async create(createUserDto: CreateUserDto) {
 		const user = { id: sequenceId++, ...createUserDto };
+		user.password = await bcrypt.hash(createUserDto.password, 5);
 		users.push(user);
 		return user;
 	}
