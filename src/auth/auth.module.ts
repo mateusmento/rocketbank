227af7cd57a490f2ee82fc35c4591096eef65123
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-05-15 17:11:21 -0300

    Include elements in AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,22 @@
 import { Module } from "@nestjs/common";
-import { LocalStrategy } from "./strategies/local.strategy";
+import { JwtModule } from "@nestjs/jwt";
+import { PassportModule } from "@nestjs/passport";
 import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
+import { LocalStrategy } from "./strategies/local.strategy";
+import { JwtStrategy } from "./strategies/jwt.strategy";
+import { UserModule } from "./../user";
 
 @Module({
-	providers: [LocalStrategy],
+	imports: [
+		UserModule,
+		PassportModule,
+		JwtModule.register({
+			secret: "secret",
+			signOptions: { expiresIn: "60m" },
+		}),
+	],
 	controllers: [AuthController],
+	providers: [AuthService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule {}
