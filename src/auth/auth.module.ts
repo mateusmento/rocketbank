acae9b329fbdbaf71bb95ecc2b51a48d3e3ce41c
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-05-15 16:24:35 -0300

    Add AuthController

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,9 @@
 import { Module } from "@nestjs/common";
 import { LocalStrategy } from "./strategies/local.strategy";
+import { AuthController } from "./auth.controller";
 
 @Module({
 	providers: [LocalStrategy],
+	controllers: [AuthController],
 })
 export class AuthModule {}
