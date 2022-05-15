b417168c75b1faa35db403ca9a411b47c55af871
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-05-15 16:35:09 -0300

    Implement signIn using authService.signIn

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,15 @@
+import { AuthService } from "./auth.service";
 import { Controller, Post, UseGuards } from "@nestjs/common";
+import { AuthUser } from "./auth-user.decorator";
 import { LocalAuthGuard } from "./guards/local.auth-guard";
 
 @Controller("auth")
 export class AuthController {
+	constructor(private readonly authService: AuthService) {}
+
 	@Post("signin")
 	@UseGuards(LocalAuthGuard)
-	signIn() {
-		return null;
+	signIn(@AuthUser() user) {
+		return this.authService.signIn(user);
 	}
 }
