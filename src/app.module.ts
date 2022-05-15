bec817382bd13c53ea4ac10d0f1ae4ba7911c9e5
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-05-15 16:20:22 -0300

    Import UserModule and AuthModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { ClientModule } from "./client/client.module";
-import { UserModule } from './user/user.module';
+import { UserModule } from "./user/user.module";
+import { AuthModule } from "./auth/auth.module";
 
 @Module({
-	imports: [ClientModule, UserModule],
+	imports: [ClientModule, UserModule, AuthModule],
 	controllers: [],
 	providers: [],
 })
