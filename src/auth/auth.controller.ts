0076209b75353dfaa252218f4b391c33345087d3
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-05-15 16:27:34 -0300

    Use password local authentication to signin endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,10 @@
 import { Controller, Post, UseGuards } from "@nestjs/common";
+import { LocalAuthGuard } from "./guards/local.auth-guard";
 
 @Controller("auth")
 export class AuthController {
 	@Post("signin")
-	@UseGuards()
+	@UseGuards(LocalAuthGuard)
 	signIn() {
 		return null;
 	}
