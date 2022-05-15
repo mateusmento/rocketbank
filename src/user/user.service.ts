e7c262ea26897c31e63c4dcb26ccea47fc9634e2
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-05-15 16:16:42 -0300

    Use User as type for mocked users

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,14 +1,17 @@
+import { User } from "./entities/user.entity";
 import { Injectable } from "@nestjs/common";
 import { CreateUserDto } from "./dto/create-user.dto";
 import * as bcrypt from "bcrypt";
 
 let sequenceId = 1;
-const users = [];
+const users: User[] = [];
 
 @Injectable()
 export class UserService {
 	async create(createUserDto: CreateUserDto) {
-		const user = { id: sequenceId++, ...createUserDto };
+		const user = new User();
+		for (const key in createUserDto) user[key] = createUserDto[key];
+		user.id = sequenceId++;
 		user.password = await bcrypt.hash(createUserDto.password, 5);
 		users.push(user);
 		return user;
