21ad3c681919205af862017dbb69a970ed39811d
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-05-15 16:18:17 -0300

    Add verifyPassword to User

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,7 +1,12 @@
+import * as bcrypt from "bcrypt";
+
 export class User {
 	id: number;
 	name: string;
 	email: string;
 	password: string;
 
+	async verifyPassword(password: string) {
+		return await bcrypt.compare(password, this.password);
+	}
 }
