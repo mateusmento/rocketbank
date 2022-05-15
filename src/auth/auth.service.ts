615f3e02c3998ee883c8b4944233b7bd988c62bc
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-05-15 16:23:28 -0300

    Move validate method of LocalStrategy to signIn in AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,13 @@
-import { Injectable } from "@nestjs/common";
+import { UserService } from "./../user/user.service";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 
 @Injectable()
 export class AuthService {
+	constructor(private readonly userService: UserService) {}
+
+	validateCredentials(email: string, password: string) {
+		const user = this.userService.findByEmail(email);
+		if (user && user.verifyPassword(password)) return user;
+		throw new UnauthorizedException();
+	}
 }
--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -1,17 +1,15 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { AuthService } from "./../auth.service";
+import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { Strategy } from "passport-local";
-import { UserService } from "src/user/user.service";
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
-	constructor(private readonly userService: UserService) {
+	constructor(private readonly authService: AuthService) {
 		super({ usernameField: "email" });
 	}
 
 	async validate(email: string, password: string) {
-		const user = this.userService.findByEmail(email);
-		if (user && user.verifyPassword(password)) return user;
-		throw new UnauthorizedException();
+		return this.authService.validateCredentials(email, password);
 	}
 }
