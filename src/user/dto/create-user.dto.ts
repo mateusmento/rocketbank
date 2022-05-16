b6134d286287eb3de371ada9ecd884a861c7a00a
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-05-16 19:27:18 -0300

    Add validation to CreateUserDto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,12 +1,17 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsEmail, MinLength } from "class-validator";
 
 export class CreateUserDto {
+	@IsNotEmpty()
 	@ApiProperty()
 	name: string;
 
+	@IsEmail()
 	@ApiProperty()
 	email: string;
 
+	@MinLength(8)
+	@IsNotEmpty()
 	@ApiProperty()
 	password: string;
 }
