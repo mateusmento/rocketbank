f7d772209ef2dedf77db3f886db307f13cd97e87
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-05-16 19:20:29 -0300

    Change port of cors enabled url

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	app.enableCors({ origin: "http://localhost:3001" });
+	app.enableCors({ origin: "http://localhost:3000" });
 
 	const config = new DocumentBuilder()
 		.setTitle("Rocket Bank API")
