28e093061aefe58699cf24381275aa0b173f64a7
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-05-15 22:45:02 -0300

    Document signin endpoint's body with SignInDto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,12 +2,22 @@ import { AuthService } from "./auth.service";
 import { Controller, Post, UseGuards } from "@nestjs/common";
 import { AuthUser } from "./auth-user.decorator";
 import { LocalAuthGuard } from "./guards/local.auth-guard";
+import { ApiBody, ApiProperty } from "@nestjs/swagger";
+
+class SignInDto {
+	@ApiProperty()
+	email: string;
+
+	@ApiProperty()
+	password: string;
+}
 
 @Controller("auth")
 export class AuthController {
 	constructor(private readonly authService: AuthService) {}
 
 	@Post("signin")
+	@ApiBody({ type: SignInDto })
 	@UseGuards(LocalAuthGuard)
 	signIn(@AuthUser() user) {
 		return this.authService.signIn(user);
