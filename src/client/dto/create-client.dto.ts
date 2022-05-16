699db3c3a9e55e4034e1c46b017b2f6d7c187af0
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-05-16 19:25:15 -0300

    Add validation for CreateClientDto

--- src/client/dto/create-client.dto.ts
+++ src/client/dto/create-client.dto.ts
@@ -1,12 +1,18 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty } from "class-validator";
+import { IsCPF } from "brazilian-class-validator";
 
 export class CreateClientDto {
+	@IsNotEmpty()
 	@ApiProperty()
 	name: string;
 
+	@IsCPF()
+	@IsNotEmpty()
 	@ApiProperty()
 	cpf: string;
 
+	@IsNotEmpty()
 	@ApiProperty()
 	birthDate: Date;
 }
