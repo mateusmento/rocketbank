23271258d549836a97a7f5add44e74ab94b3272f
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-05-15 22:42:31 -0300

    Configure swagger in bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.enableCors({ origin: "http://localhost:3001" });
+
+	const config = new DocumentBuilder()
+		.setTitle("Rocket Bank API")
+		.setDescription("RESTful API da Rocket Bank")
+		.setVersion("1.0")
+		.addTag("rocket-bank")
+		.addBearerAuth()
+		.build();
+
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup("api-doc", app, document);
+
 	await app.listen(3000);
 }
 bootstrap();
