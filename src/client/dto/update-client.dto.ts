7be28631c5763bc949127f2493759da81927644d
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-05-17 21:08:14 -0300

    Remove validation from UpdateClientDto and make fields optional

--- src/client/dto/update-client.dto.ts
+++ src/client/dto/update-client.dto.ts
@@ -1,18 +1,12 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty } from "class-validator";
-import { IsCPF } from "brazilian-class-validator";
 
 export class UpdateClientDto {
-	@IsNotEmpty()
 	@ApiProperty()
-	name: string;
+	name?: string;
 
-	@IsCPF()
-	@IsNotEmpty()
 	@ApiProperty()
-	cpf: string;
+	cpf?: string;
 
-	@IsNotEmpty()
 	@ApiProperty()
-	birthDate: Date;
+	birthDate?: Date;
 }
