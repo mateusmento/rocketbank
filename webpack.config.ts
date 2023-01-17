6be88ed874d99524f3ecdede69df2b0d87411f27
Mateus Sarmento <omateusmento@gmail.com>
2023-01-17 19:52:49 -0300

    Add initial webpack config

---
+++ webpack.config.ts
@@ -0,0 +1,26 @@
+import { Configuration } from "webpack";
+import path from "path";
+import "webpack-dev-server";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export default {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "index.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    devServer: {
+        historyApiFallback: {
+            index: "/index.html"
+        },
+        port: "auto",
+        hot: true,
+        open: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+        })
+    ]
+} as Configuration;
