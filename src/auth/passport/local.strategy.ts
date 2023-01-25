6241e69d6222404009d87a2f3afa21e986901c18
Mateus Sarmento <omateusmento@gmail.com>
2023-01-24 23:35:40 -0300

    Fix local strategy

--- src/auth/passport/local.strategy.ts
+++ src/auth/passport/local.strategy.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
-import { Strategy } from "passport-jwt";
+import { Strategy } from "passport-local";
 import { Authentication } from "../domain/authentication";
 
 @Injectable()
