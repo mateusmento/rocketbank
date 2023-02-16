7d07068ab7a9b06b2add6c5292d2566cba1bbc5f
Mateus Sarmento <omateusmento@gmail.com>
2023-02-16 07:45:13 -0300

    Implement app config validation

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,5 +1,16 @@
+import { plainToInstance } from "class-transformer";
+import { validateSync } from "class-validator";
+
 export class AppConfig {
 	static validate(config: Record<string, any>): Record<string, any> {
-		return config;
+		const appConfig = plainToInstance(AppConfig, config, {
+			enableImplicitConversion: true,
+			exposeDefaultValues: true,
+		});
+		const errors = validateSync(appConfig, {
+			skipMissingProperties: false,
+		});
+		if (errors.length > 0) throw new Error("\n" + errors.toString());
+		return appConfig;
 	}
 }
