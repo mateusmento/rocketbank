5c3beaf7032e3d1059c2b4f289d957ec7bab7079
Mateus Sarmento <omateusmento@gmail.com>
2023-02-16 07:58:14 -0300

    Set cors origin using app config

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { ValidationPipe } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	app.enableCors({ origin: "*" });
+
+	const configService = app.get(ConfigService);
+
+	app.enableCors({
+		origin: configService.get("APP_URL"),
+		credentials: true,
+	});
+
 	app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
 	const config = new DocumentBuilder()
