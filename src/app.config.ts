6603c066e746104623584425426261d6a628b143
Mateus Sarmento <omateusmento@gmail.com>
2023-02-16 07:52:06 -0300

    Add app config properties

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,7 +1,31 @@
 import { plainToInstance } from "class-transformer";
-import { validateSync } from "class-validator";
+import { IsNumber, IsString, validateSync } from "class-validator";
 
 export class AppConfig {
+	@IsString()
+	DATABASE_HOST: string;
+
+	@IsString()
+	DATABASE_PORT: string;
+
+	@IsString()
+	POSTGRES_USER: string;
+
+	@IsString()
+	POSTGRES_PASSWORD: string;
+
+	@IsString()
+	POSTGRES_DB: string;
+
+	@IsNumber()
+	PASSWORD_SALT_ROUNDS: number;
+
+	@IsNumber()
+	JWT_COOKIE_MAX_AGE: number;
+
+	@IsNumber()
+	JWT_EXPIRES_IN: number;
+
 	static validate(config: Record<string, any>): Record<string, any> {
 		const appConfig = plainToInstance(AppConfig, config, {
 			enableImplicitConversion: true,
