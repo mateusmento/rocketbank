a89e76c7e4e4c62678be671abbb731de1735f92d
Mateus Sarmento <omateusmento@gmail.com>
2023-02-18 09:22:15 -0300

    Add hostname and https to dev server

--- webpack.config.ts
+++ webpack.config.ts
@@ -4,41 +4,46 @@ import "webpack-dev-server";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
 export default {
-    entry: "./src/index.jsx",
-    output: {
-        filename: "index.js",
-        path: path.resolve(__dirname, "dist"),
-    },
-    resolve: {
-        extensions: [".js", ".jsx"],
-    },
-    devServer: {
-        historyApiFallback: {
-            index: "/index.html"
-        },
-        // host: "app.rocketbank.com",
-        port: "auto",
-        hot: true,
-        open: true,
-        static: "./src/assets"
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html",
-            favicon: false,
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: "babel-loader"
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            }
-        ]
-    }
+	entry: "./src/index.jsx",
+	output: {
+		filename: "index.js",
+		path: path.resolve(__dirname, "dist"),
+	},
+	resolve: {
+		extensions: [".js", ".jsx"],
+	},
+	devServer: {
+		hot: true,
+		open: true,
+		host: "app.rocketbank.com",
+		port: "auto",
+		historyApiFallback: true,
+		static: "./src/assets",
+		server: {
+			type: "https",
+			options: {
+				key: "./https/app.key.pem",
+				cert: "./https/app.pem",
+			},
+		},
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/index.html",
+			favicon: false,
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: "babel-loader"
+			},
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"]
+			}
+		]
+	}
 } as Configuration;
