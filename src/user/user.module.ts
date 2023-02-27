98f1648b52598fef8cd945a1125dbec52c1c0eca
Mateus Sarmento <omateusmento@gmail.com>
2023-02-27 09:31:09 -0300

    Add UserCredential to UserModule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,10 @@ import { Module } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { UserController } from "./user.controller";
 import { User } from "./entities/user.entity";
+import { UserCredential } from "./entities/user-credential.entity";
 
 @Module({
-	imports: [TypeOrmModule.forFeature([User])],
+	imports: [TypeOrmModule.forFeature([User, UserCredential])],
 	controllers: [UserController],
 	providers: [UserService],
 	exports: [UserService],
