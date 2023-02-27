b6ee81b63feb3e73e8bb71ef27ebaac59729d258
Mateus Sarmento <omateusmento@gmail.com>
2023-02-27 08:42:02 -0300

    Fix update client dto

--- src/client/dto/update-client.dto.ts
+++ src/client/dto/update-client.dto.ts
@@ -1,12 +1,16 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { Allow } from "class-validator";
 
 export class UpdateClientDto {
+	@Allow()
 	@ApiProperty()
 	name?: string;
 
+	@Allow()
 	@ApiProperty()
 	cpf?: string;
 
+	@Allow()
 	@ApiProperty()
 	birthDate?: Date;
 }
